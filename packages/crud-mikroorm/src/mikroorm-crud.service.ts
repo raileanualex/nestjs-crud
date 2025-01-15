7865a32cea52cb4ff5199617059fccfe13ef680c
Alexandru Raileanu <raileanualex03@gmail.com>
2025-01-15 22:06:33 +0200

    feat: add onInitMapEntityColumns

--- packages/crud-mikroorm/src/mikroorm-crud.service.ts
+++ packages/crud-mikroorm/src/mikroorm-crud.service.ts
@@ -1,24 +1,56 @@
 import {
   CreateManyDto,
   CrudRequest,
+  CrudRequestOptions,
   CrudService,
   GetManyDefaultResponse,
 } from '@n4it/crud';
 
-import { EntityData, EntityManager, EntityRepository, Populate } from '@mikro-orm/core';
+import { EntityData, EntityManager, EntityRepository, FilterQuery, Populate } from '@mikro-orm/core';
 import { BadRequestException } from '@nestjs/common/exceptions';
+import { ParsedRequestParams } from '@n4it/crud-request/interfaces';
+import { ObjectLiteral } from '@n4it/crud-util';
 
 export class MikroOrmCrudService<T extends object, DTO extends EntityData<T> = EntityData<T>> extends CrudService<T, EntityData<T>> {
+  protected entityColumns: string[];
+  protected entityPrimaryColumns: string[];
+  protected entityName: string;
+  protected entityColumnsHash: ObjectLiteral = {};
+
   constructor(
       private readonly em: EntityManager,
       private readonly entity: { new(): T },
   ) {
       super();
+      this.entityName = entity.name;
+
+      this.onInitMapEntityColumns();
   }
+
 
   private get repository(): EntityRepository<T> {
       return this.em.getRepository(this.entity);
   }
+
+  protected onInitMapEntityColumns() {
+    const metadata = this.em.getMetadata().get(this.entityName);
+  
+    this.entityColumns = Object.values(metadata.properties).map((prop) => {
+      // Check if the property is embedded
+      if (prop.embedded) {
+          this.entityColumnsHash[prop.name] = prop.name; // Use the full path for embedded properties
+          return prop.name;
+      }
+      this.entityColumnsHash[prop.name] = prop.name; // Regular properties
+      return prop.name;
+  });
+
+  // Identify primary columns
+  this.entityPrimaryColumns = Object.values(metadata.properties)
+      .filter((prop) => prop.primary) // Check if the property is a primary key
+      .map((prop) => prop.name);
+  }
+  
 
   async getMany(req: CrudRequest): Promise<GetManyDefaultResponse<T> | T[]> {
       const { parsed, options } = req;
@@ -137,6 +169,91 @@ export class MikroOrmCrudService<T extends object, DTO extends EntityData<T> = E
 
   transform(crudRequest: CrudRequest): any {
     
+  
+
+  protected getSelect(query: ParsedRequestParams, options: QueryOptions): string[] {
+    const allowed = this.getAllowedColumns(this.entityColumns, options);
+
+    const columns =
+      query.fields && query.fields.length
+        ? query.fields.filter((field) => allowed.some((col) => field === col))
+        : allowed;
+
+    const select = [
+      ...new Set([
+        ...(options.persist && options.persist.length ? options.persist : []),
+        ...columns,
+        ...this.entityPrimaryColumns,
+      ]),
+    ].map((col) => `${this.alias}.${col}`);
+
+    return Array.from(new Set(select));
+  }
+
+  public async createBuilder(
+    parsed: ParsedRequestParams,
+    options: CrudRequestOptions,
+    many = true,
+    withDeleted = false,
+  ): Promise<{ filter: FilterQuery<T>; options: any }> {
+    // Initialize the filter and options
+    const filter: FilterQuery<T> = {};
+    const queryOptions: any = {};
+  
+    // Get selected fields
+    const select = this.getSelect(parsed, options.query);
+    if (select && select.length) {
+      queryOptions.fields = select;
+    }
+  
+    // Add search conditions
+    this.setSearchCondition(filter, parsed.search, options.operators?.custom);
+  
+    // Set joins (populate in MikroORM)
+    const joinOptions = options.query?.join || {};
+    const allowedJoins = Object.keys(joinOptions);
+  
+    if (allowedJoins.length > 0) {
+      queryOptions.populate = queryOptions.populate || [];
+      const eagerJoins: Record<string, boolean> = {};
+  
+      for (const join of allowedJoins) {
+        if (joinOptions[join].eager) {
+          queryOptions.populate.push(join as any);
+          eagerJoins[join] = true;
+        }
+      }
+  
+      if (parsed.join?.length) {
+        for (const join of parsed.join) {
+          if (!eagerJoins[join.field] && allowedJoins.includes(join.field)) {
+            queryOptions.populate.push(join.field as any);
+          }
+        }
+      }
+    }
+  
+    if (many) {
+      // Set sort (order by)
+      const sort = this.getSort(parsed, options.query) as QueryOrderMap;
+      if (sort) {
+        queryOptions.orderBy = sort;
+      }
+  
+      // Set take (limit)
+      const take = this.getTake(parsed, options.query);
+      if (take && isFinite(take)) {
+        queryOptions.limit = take;
+      }
+  
+      // Set skip (offset)
+      const skip = this.getSkip(parsed, take);
+      if (skip && isFinite(skip)) {
+        queryOptions.offset = skip;
+      }
+    }
+  
+    return { filter, options: queryOptions };
   }
 
 }
--- packages/crud-request/src/request-query.parser.ts
+++ packages/crud-request/src/request-query.parser.ts
@@ -321,6 +321,7 @@ export class RequestQueryParser implements ParsedRequestParams {
     customOperators: CustomOperators,
     data: string,
   ): QueryFilter {
+    console.log("LOG=", data);
     const isArrayValue = [
       'in',
       'notin',
