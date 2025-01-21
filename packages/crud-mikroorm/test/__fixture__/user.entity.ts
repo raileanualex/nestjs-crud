14dc09d53c8e37048c925969b75da6117b8a56fa
Alexandru Raileanu <raileanualex03@gmail.com>
2025-01-21 13:53:40 +0200

    chore: adjust table name in test

--- packages/crud-mikroorm/test/__fixture__/user.entity.ts
+++ packages/crud-mikroorm/test/__fixture__/user.entity.ts
@@ -1,6 +1,6 @@
 import { Entity, PrimaryKey, Property } from '@mikro-orm/core';
 
-@Entity({ tableName: 'users2' })
+@Entity({ tableName: 'users' })
 export class User {
   @PrimaryKey()
   id?: number;
